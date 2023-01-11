7dc77c1deec8eb35d49f2d5653ff5783bed533f4
Clau <Claudiapbrasca@gmail.com>
2023-01-11 13:33:54 +0100

    first_terraform_file

---
+++ first_terraform.tf
@@ -0,0 +1,14 @@
+# Configure the AWS Provider
+provider "aws" {
+        region = "us-east-1"
+        access_key = "number" #está en AWS, NO HARDCODEAR
+        secret_key = "secret_key" #está en AWS se tiene que descargar, NO HARDCODEAR
+}
+
+# Create a VPC
+resource "aws_instance" "my-first-server" {
+        ami = "ami-0000000000imag3"
+        instance_type = "t2.micro"
+
+}
+
